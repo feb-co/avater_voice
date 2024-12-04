9d8aadb2514b9b0367d0745db0994c1fd508bfa2
Licheng Wang <244267620@qq.com>
2024-12-04 21:10:59 +0000

    update code

--- model/llama_tts/configuration_llama_tts.py
+++ model/llama_tts/configuration_llama_tts.py
@@ -1,16 +1,17 @@
-"""LLaMA TTS Adapter model configuration"""
+"""LLaMA TTS model configuration"""
 
 from transformers import LlamaConfig
 
 
-class LlamaTTSApaterConfig(LlamaConfig):
-    model_type = "llama tts adapter"
+class LlamaTTSConfig(LlamaConfig):
+    model_type = "llama tts"
 
     def __init__(
         self,
         audio_vocab_size=32000,
         adapter_hidden_layers=6,
-        adapter_attention_heads=8,
+        adapter_attention_heads=16,
+        dropout=0.0,
         boa_token_id=1,
         eoa_token_id=2,
         **kwargs,
@@ -18,6 +19,7 @@ class LlamaTTSApaterConfig(LlamaConfig):
         self.audio_vocab_size = audio_vocab_size
         self.adapter_hidden_layers = adapter_hidden_layers
         self.adapter_attention_heads = adapter_attention_heads
+        self.dropout = dropout
         self.boa_token_id = boa_token_id
         self.eoa_token_id = eoa_token_id
 
