c0b2e6d2a5523be38f2f114709e5acebccb723ff
Licheng Wang <244267620@qq.com>
2025-03-16 07:51:21 +0000

    update code

--- infer/avater_infer/models/patcher.py
+++ infer/avater_infer/models/patcher.py
@@ -4,7 +4,7 @@ from transformers.generation.logits_process import TopPLogitsWarper
 from transformers import PreTrainedModel, PreTrainedTokenizer
 from transformers.generation import GenerationMixin
 
-from ..generation.utils import validate_model_kwargs
+from ..generation_utils import validate_model_kwargs
 
 
 def patch_model(model: "PreTrainedModel", tokenizer: "PreTrainedTokenizer"):
